51f63937ed77f725974539ad9c2e8e01593bc377
Idhrendur <idhrendur@hotmail.com>
2018-04-28 11:09:07 -0700

    Update the build.bat to actually work

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
-copy Converter_Files/* "D:\My Projects\paradoxGameConverters - Vic2ToHoI4\Vic2ToHoI4\Release\"
+xcopy Converter_Files "D:\My Projects\paradoxGameConverters - Vic2ToHoI4\Vic2ToHoI4\Release\" /Y /Q
 copy Saves\The_Roman_Empire1936_01_01.v2 "D:\My Projects\paradoxGameConverters - Vic2ToHoI4\Vic2ToHoI4\Release\"
 cd "D:\My Projects\paradoxGameConverters - Vic2ToHoI4\Vic2ToHoI4\Release\"
 V2ToHoI4Converter.exe The_Roman_Empire1936_01_01.v2
-xcopy "output\The_Roman_Empire1936_01_01" "D:\My Projects\RomanHoI4/results"
+xcopy "output\The_Roman_Empire1936_01_01" "D:\My Projects\RomanHoI4\results\The_Roman_Empire1936_01_01" /E /Y /I /Q
+copy "output\The_Roman_Empire1936_01_01.mod" "D:\My Projects\RomanHoI4\results\"
+cd "D:\My Projects\RomanHoI4"
+xcopy HoI4Mod results /E /Y /I /Q
