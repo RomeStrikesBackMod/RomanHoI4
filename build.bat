dbf63674cd0eb61fc932322b31478ec33fdbf2ec
Idhrendur <idhrendur@hotmail.com>
2018-12-31 20:29:42 -0800

    Make build file use new converter location.

--- build.bat
+++ build.bat
@@ -1,9 +1,10 @@
 del results /S /Q
 rmdir results /S /Q
 mkdir results
-xcopy Converter_Files "D:\My Projects\paradoxGameConverters - Vic2ToHoI4\Vic2ToHoI4\Release\" /Y /Q
-copy Saves\The_Roman_Empire1936_01_01.v2 "D:\My Projects\paradoxGameConverters - Vic2ToHoI4\Vic2ToHoI4\Release\"
-cd "D:\My Projects\paradoxGameConverters - Vic2ToHoI4\Vic2ToHoI4\Release\"
+
+xcopy Converter_Files "D:\My Projects\paradoxGameConvertersForks\Vic2ToHoI4\Release\Vic2ToHoI4" /Y /Q
+copy Saves\The_Roman_Empire1936_01_01.v2 "D:\My Projects\paradoxGameConvertersForks\Vic2ToHoI4\Release\Vic2ToHoI4"
+cd "D:\My Projects\paradoxGameConvertersForks\Vic2ToHoI4\Release\Vic2ToHoI4"
 V2ToHoI4Converter.exe The_Roman_Empire1936_01_01.v2
 xcopy "output\The_Roman_Empire1936_01_01" "D:\My Projects\RomanHoI4\results\The_Roman_Empire1936_01_01" /E /Y /I /Q
 copy "output\The_Roman_Empire1936_01_01.mod" "D:\My Projects\RomanHoI4\results\"
