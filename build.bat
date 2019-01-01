9f09f038bc16e0d3f224df5d378986a2b708ff08
Idhrendur <idhrendur@hotmail.com>
2018-12-31 23:24:43 -0800

    Improve some more country history files.

--- build.bat
+++ build.bat
@@ -13,3 +13,9 @@ cd "D:\My Projects\RomanHoI4"
 xcopy HoI4Mod results /E /Y /I /Q
 
 "c:\Program Files (x86)\Beyond Compare 4\Patch.exe" "results/The_Roman_Empire1936_01_01/history/countries/ALG - Algeria.txt" "HoI4Mod/patch/AlgeriaHistory.patch"
+"c:\Program Files (x86)\Beyond Compare 4\Patch.exe" "results/The_Roman_Empire1936_01_01/history/countries/ANG - Angola.txt" "HoI4Mod/patch/AngolaHistory.patch"
+"c:\Program Files (x86)\Beyond Compare 4\Patch.exe" "results/The_Roman_Empire1936_01_01/history/countries/AOT - Aoteorea.txt" "HoI4Mod/patch/AoteoreaHistory.patch"
+"c:\Program Files (x86)\Beyond Compare 4\Patch.exe" "results/The_Roman_Empire1936_01_01/history/countries/AQI - Aquitaine.txt" "HoI4Mod/patch/AquitaineHistory.patch"
+"c:\Program Files (x86)\Beyond Compare 4\Patch.exe" "results/The_Roman_Empire1936_01_01/history/countries/ARM - Armenia.txt" "HoI4Mod/patch/ArmeniaHistory.patch"
+"c:\Program Files (x86)\Beyond Compare 4\Patch.exe" "results/The_Roman_Empire1936_01_01/history/countries/AST - Australia.txt" "HoI4Mod/patch/AustraliaHistory.patch"
+"c:\Program Files (x86)\Beyond Compare 4\Patch.exe" "results/The_Roman_Empire1936_01_01/history/countries/AZR - Azerbaijan.txt" "HoI4Mod/patch/AzerbaijanHistory.patch"
