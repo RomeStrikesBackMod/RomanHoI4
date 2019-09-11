7adf430ceb1a4d5ddf78246ed723f2cd1e9c5087
Idhrendur <idhrendur@hotmail.com>
2019-09-10 21:24:34 -0700

    Remove old results at the right time.

--- build.bat
+++ build.bat
@@ -1,11 +1,12 @@
+del results /S /Q
+rmdir results /S /Q
+mkdir results
+
 xcopy Converter_Files "D:\MyProjects\paradoxGameConvertersForks\Vic2ToHoI4\Release\Vic2ToHoI4" /Y /Q /S
 copy Saves\The_Roman_Empire1936_01_01.v2 "D:\MyProjects\paradoxGameConvertersForks\Vic2ToHoI4\Release\Vic2ToHoI4"
 cd "D:\MyProjects\paradoxGameConvertersForks\Vic2ToHoI4\Release\Vic2ToHoI4"
 V2ToHoI4Converter.exe The_Roman_Empire1936_01_01.v2
 
-del results /S /Q
-rmdir results /S /Q
-mkdir results
 xcopy "output\The_Roman_Empire1936_01_01" "D:\MyProjects\RomanHoI4\results\The_Roman_Empire1936_01_01" /E /Y /I /Q
 copy "output\The_Roman_Empire1936_01_01.mod" "D:\MyProjects\RomanHoI4\results\"
 
