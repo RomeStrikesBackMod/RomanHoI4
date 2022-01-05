fbe617b030de41e7b2d8a8f882c45f09b4c66b1c
Stephen May <idhrendur@hotmail.com>
2022-01-04 23:37:22 -0800

    Replace Vic2 mod before building to ensure its up to date

--- build.bat
+++ build.bat
@@ -2,6 +2,10 @@ del results /S /Q
 rmdir results /S /Q
 mkdir results
 
+del "C:\Program Files (x86)\Steam\steamapps\common\Victoria 2\mod\TheEmpireStrikesBack" /S /Q
+rmdir "C:\Program Files (x86)\Steam\steamapps\common\Victoria 2\mod\TheEmpireStrikesBack" /S /Q
+xcopy Vic2Mod "C:\Program Files (x86)\Steam\steamapps\common\Victoria 2\mod\" /Y /Q /S
+
 xcopy Converter_Files "C:\MyProjects\paradoxGameConvertersForks\Vic2ToHoI4\Release\Vic2ToHoI4" /Y /Q /S
 cd "C:\MyProjects\paradoxGameConvertersForks\Vic2ToHoI4\Release\Vic2ToHoI4"
 V2ToHoI4Converter.exe
