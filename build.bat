074a4872cc7b62d37dc729bc07ddf27409a5a477
Stephen May <idhrendur@hotmail.com>
2022-01-15 21:13:46 -0800

    Improve state localisations

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ mkdir results
 
 xcopy Converter_Files "C:\MyProjects\paradoxGameConvertersForks\Vic2ToHoI4\Release\Vic2ToHoI4" /Y /Q /S
 cd "C:\MyProjects\paradoxGameConvertersForks\Vic2ToHoI4\Release\Vic2ToHoI4"
-V2ToHoI4Converter.exe
+Vic2ToHoI4Converter.exe
 
 xcopy "output\Rome_Strikes_Back" "C:\MyProjects\RomanHoI4\results\Rome_Strikes_Back" /E /Y /I /Q
 copy "output\Rome_Strikes_Back.mod" "C:\MyProjects\RomanHoI4\results\"
