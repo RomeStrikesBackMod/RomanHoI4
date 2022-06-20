fb54cf606df49e9dfc5e24e742d92283d0c313d0
Idhrendur <idhrendur@hotmail.com>
2022-06-20 13:01:50 -0700

    Incorporate latest converter updates (#123)

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@ del results /S /Q
 rmdir results /S /Q
 mkdir results
 
-xcopy Converter_Files "C:\MyProjects\paradoxGameConvertersForks\Vic2ToHoI4\Release\Vic2ToHoI4" /Y /Q /S
-cd "C:\MyProjects\paradoxGameConvertersForks\Vic2ToHoI4\Release\Vic2ToHoI4"
+xcopy Converter_Files "C:\MyProjects\paradoxGameConvertersForks\Vic2ToHoI4\build\Release\Vic2ToHoI4" /Y /Q /S
+cd "C:\MyProjects\paradoxGameConvertersForks\Vic2ToHoI4\build\Release\Vic2ToHoI4"
 Vic2ToHoI4Converter.exe
 
 xcopy "output\Rome_Strikes_Back" "C:\MyProjects\RomanHoI4\results\Rome_Strikes_Back" /E /Y /I /Q
